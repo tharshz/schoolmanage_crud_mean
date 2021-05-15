de2e9495241fd0abcf4118d90c628df10f523513
tharshz <tharshzlog@gmail.com>
2021-05-15 09:17:05 +0530

    first commit

---
+++ backend/database/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+  db: 'mongodb+srv://tharshz:bmwi8@cluster0.ri3ia.mongodb.net/schoolsystem?retryWrites=true&w=majority'
+};
---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FilterPipe } from './pipes/filter.pipe';
+
+import { AddStudentComponent } from './components/add-student/add-student.component';
+import { EditStudentComponent } from './components/edit-student/edit-student.component';
+import { StudentsListComponent } from './components/students-list/students-list.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './material.module';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+
+import { HttpClientModule } from '@angular/common/http';
+
+
+import { ApiService } from './shared/api.service';
+
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AddStudentComponent,
+    EditStudentComponent,
+    StudentsListComponent,
+    FilterPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  providers: [ApiService],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+
+export class AppModule { }
